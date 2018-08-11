bf73d107735a399b73df02d07f128d6b48c4585b
Myroslav Andreykiv <andreykiv09@gmail.com>
2018-08-11 09:31:57 +0000

    CREATE and NEW routes added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 var Book = require("./models/book");
+var expressSanitizer = require('express-sanitizer');
 
 
 // APP CONFIG
@@ -12,6 +13,7 @@ mongoose.connect("mongodb://localhost/bookabulary");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 // APP ROUTES
@@ -40,7 +42,18 @@ app.get("/books/new", function(req, res){
     res.render("new");
 });
 
+// create - import data from new template to DB
 
+app.post("/books", function(req, res){
+req.body.book.body = req.sanitize(req.body.book.body);
+    Book.create(req.body.book, function(err, newBook){
+        if(err){
+            res.render('new');
+        } else {
+            res.redirect("/books");
+        }
+    });
+});
 
 
 
